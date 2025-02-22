19da66c7be88f809c8d7130b5eb98ea24aa70f28
vdragon1225 <148491922+vdragon1225@users.noreply.github.com>
2025-02-22 04:23:59 +0000

    feat: Print "Welcome to Java" five times

---
+++ Welcome5Times.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+Author: Daniel Lai
+Date: 1/23/25
+File: Welcome5Times.java
+*******************************************************************************/
+public class Welcome5Times
+{
+	public static void main(String[] args) {
+	    // enter your code here
+		for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+	}
+}
